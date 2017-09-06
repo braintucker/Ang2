692f8c3ae278212048f111c688dff8adb9ccd202
Brain Tucker <brian.briantucker@gmail.com>
2017-09-06 12:48:34 -0500

    added timer using Angular's change detection

--- app.js
+++ app.js
@@ -14,6 +14,13 @@
     getRandomAnimal: function() {
       var randomIndex = Math.floor(Math.random() * this.animals.length);
       return this.animals[randomIndex];
+    },
+    generateRandomAnimals: function(delay, callback){
+      var self = this;
+      callback(this.getRandomAnimal());
+      setTimeout(function(){
+        callback(self.getRandomAnimal());
+      }, delay);
     }
   });
 
@@ -22,8 +29,11 @@
     template: '<p><em>{{animal.breed}}</em> - {{animal.location}}</p>'
   })
   .Class({
-    constructor: [AnimalService, function RandomQuoteComponent(animalService) {
-      this.animal = animalService.getRandomAnimal();
+    constructor: [AnimalService, function RandomAnimalComponent(animalService) {
+      var self = this;
+      animalService.generateRandomAnimals(2000, function(animal){
+        self.animal = animal;
+      });
     }]
   });
 
@@ -52,43 +62,23 @@
   var sampleAnimals = [
     {
       "breed": "Tiger",
-      "location": "Brian W. Kernighan"
+      "location": "N/A"
     },
     {
       "breed": "Lions",
-      "location": "Edward V Berard"
+      "location": "N/A"
     },
     {
       "breed": "Bears",
-      "location": "Hofstadter's Law"
+      "location": "N/A"
     },
     {
-      "breed": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
-      "location": "Rick Osborne"
+      "breed": "Squirrel",
+      "location": "N/A"
     },
     {
-      "breed": "In theory, there is no difference between theory and practice. But, in practice, there is.",
-      "location": "Jan L. A. van de Snepscheut"
-    },
-    {
-      "breed": "Measuring programming progress by breeds of code is like measuring aircraft building progress by weight.",
-      "location": "Bill Gates"
-    },
-    {
-      "breed": "There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.",
-      "location": "Leon Bambrick"
-    },
-    {
-      "breed": "Nine people can't make a baby in a month.",
-      "location": "Fred Brooks"
-    },
-    {
-      "breed": "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
-      "location": "Edsger Dijkstra"
-    },
-    {
-      "breed": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
-      "location": "Tom Cargill"
+      "breed": "Humans",
+      "location": "N/A"
     }
   ];
 
